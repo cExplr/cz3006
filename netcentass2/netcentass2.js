cc20e8499bf56a175374be2dfba315c4656fb5de
cExplr <code.explore.95@gmail.com>
2018-04-13 17:58:40 +0800

    UNDER PROCESS.PHP :    The file writing and updating to file has been done including flock. Comments are also filled in
    order.txt is needed tio store the records in
    
    netcentass2.js is also modified, removing redundant code and added comments in

--- netcentass2/netcentass2.js
+++ netcentass2/netcentass2.js
@@ -1,16 +1,16 @@
 
-var outputText = document.getElementById("outputPriceText");
+
 
 
 /*Initialization*/
 
-var bQtyTextBox=0; // Tetxtbox of Banana
-var oQtyTextBox=0; // Textbox of Orange
-var aQtyTextBox=0; // Textbox of Apple
+var outputText = document.getElementById("outputPriceText"); // Textbox of username
 
 function checkIfNonZero(fruit){
 	var qty = 0;
 	switch (fruit){
+		// Depending on fruits, we want to get the quantity of that specific input
+		//So i used the switch case control flow here
 		case 'Apple':
 			qty=document.getElementById("AppleQtyText").value;
 			break;
@@ -24,10 +24,12 @@ function checkIfNonZero(fruit){
 		default:
 			alert("ERROR! NO SUCH FRUIT 1");
 	}
+
+	//If quantity is 0 or is backspaced with no input
 	if ( qty == 0 || qty==null){
 		var textbox = fruit +  "QtyText";
 		//alert(textbox);
-		document.getElementById(textbox).value="0";
+		document.getElementById(textbox).value="0";  // Add back the default value of 0
 	}
 
 	/*
@@ -44,32 +46,36 @@ function checkIfNonZero(fruit){
 /*Event handler when submit button is clicked*/
 
 /*On key up to check qty*/
-
-
 function checkQty(fruit){
-	var submitbutton = document.getElementById("submitButton");
+	//Initialization
+	var submitbutton = document.getElementById("submitButton"); // Get the submit button object
+
 
 	var qtyB = document.getElementById("BananaQtyText").value;
-
 	var qtyO = document.getElementById("OrangeQtyText").value;
-		
 	var qtyA = document.getElementById("AppleQtyText").value;
 
-	var okB = qtyB.search(/^[0-9]*$/);	// Make sure it is just digits
+
+	// Regex to check that the inputs are purely integers since no decimal point should be allowed
+	var okB = qtyB.search(/^[0-9]*$/);
 	var okO = qtyO.search(/^[0-9]*$/);
 	var okA = qtyA.search(/^[0-9]*$/);
+
 	if (okB != 0 || qtyB < 0 || okA != 0 || qtyA < 0 ||okO != 0 || qtyO < 0){
-		submitbutton.disabled = true;
-		output("");
-		output("NaN\n\nQuantity field only accepts digits");
+		//Anything that does not fit in the described regex  will come here
+		submitbutton.disabled = true;	// The submit button wil be disabled
+		output("");						//Clear outputTextBox
+		output("NaN\n\nQuantity field only accepts digits");  // Display Error Message
+
 	}else
+	//IF NO FRUITS WERE BOUGHT
 	if (qtyA == 0 && qtyB == 0 && qtyO ==0){
 		output("");
-		submitbutton.disabled = false;
+		submitbutton.disabled = false;	//Submit buton will still be pressable
 	}
 	else{
-		displayTotalCost(qtyA, qtyB, qtyO);
-		submitbutton.disabled = false;
+		displayTotalCost(qtyA, qtyB, qtyO);	// If nothing goes wrong, then display total cost of items
+		submitbutton.disabled = false;		//Make sure submit button is pressable
 	}
 	
 }
@@ -77,34 +83,46 @@ function checkQty(fruit){
 function displayTotalCost(a,b,o){
 	totalCost = a*0.75+b*0.39+o*0.59;
 	//output("\nNo of Apples : " + a+"\n\nNo of Oranges : " + o+"\n\nNo of Bananas : " + b+"\n\nTotal Cost of Fruits : s$" + (totalCost.toFixed(2)) +"\n\n");
+	//toFixed(2) allows us to write floating point values to 2 dp
 	var outputMessage = "No of Apples : " + a+"\nNo of Oranges : " + o+"\nNo of Bananas : " + b+"\nTotal Cost of Fruits : s$" + totalCost.toFixed(2)+"\n";
-	output(outputMessage);
+	output(outputMessage); // Print the outputMessage
 
 }
 
 function output(outputvalue){
 	var outputText = document.getElementById("outputPriceText");
-	//alert("This is no ok");
-	outputText.value = outputvalue;
+	//alert("This is no ok"); // DEBUGGING PIRPOSE
+	outputText.value = outputvalue;   //Display message into the otputtextbox
 }
 
+/*WHEN THE SUBMIT BUTTON IS PRESSED */
 function onSubmitButtonPressed(){
+
+	//vairable initialization
 	var totalQty=0;
 	var paymentMode="";
-	/*Get the quantities from textboxes*/
+
+	/*Get the textboxes object */
 	bQtyTextBox = document.getElementById("BananaQtyText");
 	oQtyTextBox = document.getElementById("OrangeQtyText");
 	aQtyTextBox = document.getElementById("AppleQtyText");
+	usernameTextBox = document.getElementById("usernameText");
 
-	totalQty= bQtyTextBox.value + oQtyTextBox.value + aQtyTextBox.value;
-	if(totalQty == 0){
-		alert("There are no fruits being added!");
+
+	totalQty= bQtyTextBox.value + oQtyTextBox.value + aQtyTextBox.value; // Calculate the total quantity of all the fruits
+
+	if(totalQty == 0){				// Check if any number of fruits is added
+		alert("There are no fruits being added!");	//This is the reason why we still allow the buttpon to be pressable
 	}
 	else{
+
+		//Initialize variables of radiobuttons
 		var payBy = "";
 		var visa = document.getElementById("modeV");
 		var discovery = document.getElementById("modeD");
 		var mastercard = document.getElementById("modeM");
+
+		//Finding out which radiobutton has been checked
 		if (visa.checked){
 				payBy = visa.value;
 		}
@@ -115,16 +133,9 @@ function onSubmitButtonPressed(){
 			payBy = mastercard.value;
 		}
 
-		totalCost = aQtyTextBox.value*0.75+bQtyTextBox.value*0.39+oQtyTextBox.value*0.59;
+		totalCost = aQtyTextBox.value*0.75+bQtyTextBox.value*0.39+oQtyTextBox.value*0.59;  // Calculate the total cost of all the fruits
+
 		var outputMessage = "No of Apples : " + aQtyTextBox.value+"\nNo of Oranges : " + oQtyTextBox.value+"\nNo of Bananas : " + bQtyTextBox.value+"\nTotal Cost of Fruits : s$" + totalCost.toFixed(2)+"\n\n Payment By : "+ payBy;
-
-		confirm("Confirm \n\n"+outputMessage);
-
-	
-		
-		
+		alert("Dear \n"+ usernameTextBox.value + "\n\n"+outputMessage); // Inform user about the order list
 	}
-	
-
-
 }
--- netcentass2/process.php
+++ netcentass2/process.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 	<!-- 
 
     font-family: 'Black Han Sans', sans-serif;
@@ -23,42 +24,49 @@
 		table{
 			margin-left :  auto;
 			margin-right: auto;
+			margin-top: 0;
 		}
 		table, th, td {
    		border: 1px solid black;
 		border-collapse: collapse;
-		
 		}
 
 		th, td {
     		padding: 5px;
 			text-align:center; 
-			  
 		}
 
 		#TitleOfOrderSummary{
-			font-size: 3em;
+			font-size:5em;
 			text-align: center;
-			
+			margin-top: 0;
 		}
 </style>
 </head>
-
-
 <body>
 
-	<?php 
-	$qtyO= $_POST["noOranges"] ;
-	$qtyB=$_POST["noBananas"];
-	$qtyA=$_POST["noApples"];
-	$totalCostOrange = ($qtyO * 0.59);
-	$totalCostBanana = ($qtyB * 0.39);
-	$totalCostApple = ($qtyA * 0.75);
-	$totalCostOfFruits = $totalCostApple + $totalCostBanana + $totalCostOrange;
+		<?php 
+
+		//INITIALIZATIONS
+
+			//Get Quantity of fruits
+			$qtyO= $_POST["noOranges"] ;
+			$qtyB=$_POST["noBananas"];
+			$qtyA=$_POST["noApples"];
+
+			//Calculate total cost of each fruit category
+			$totalCostOrange = ($qtyO * 0.59);
+			$totalCostBanana = ($qtyB * 0.39);
+			$totalCostApple = ($qtyA * 0.75);
+
+			//Calculate Total cost of all fruits
+			$totalCostOfFruits = $totalCostApple + $totalCostBanana + $totalCostOrange;
 	?>
+
+
 	<div id="TitleOfOrderSummary">ORDER SUMMARY</div><br><br>
 	
-	<table class="summary" style="width : 80%;">
+	<table class="summary" style="width :50%;">
 		<tr >
 			<th colspan="4">
 				<?php printf("%s",$_POST["inputUsername"]); ?>
@@ -66,7 +74,7 @@
 			
 		</tr>
 		<tr>
-			<t rowspan="4";h>-</th>
+			<th rowspan="4";>-</th>
 			<th>Fruit</th>
 			<th>Quantity</th>
 			<th>Cost</th>
@@ -98,5 +106,73 @@
 		</tr>
 	</table>
 
+
+<?php 
+
+	//define file path
+	$filePath = "order.txt";
+
+	if(file_exists($filePath)){	// Check if file at filepath exists
+
+		
+		$orderfptr = fopen($filePath,'r')  or exit("Unable to open file!");		// READ the content inside after opening
+
+		if(flock($orderfptr, LOCK_SH))	//Since we allow multiple users to read the file, we set flock as LOCK_SH (shared)
+		{
+		// Get the content of the file and store into $ordersContent
+		$ordersContent = fread($orderfptr,filesize($filePath)+1);
+
+		//int preg_match ( string $pattern , string $subject  [, array &$matches [, int $flags = 0 [, int $offset = 0 ]]] )
+		//With regex, we attempt to find the pattern in the content in ordersContent and use the grouping to store the result as an array
+		//WHERE recoreddxxxxx[0] is the string and recordedxxxxx[1] is the digits or the quantity
+		preg_match("/Total number of apples: (\d+)/", $ordersContent, $recordedAppleNumber);
+		preg_match("/Total number of oranges: (\d+)/", $ordersContent, $recordedOrangeNumber);
+		preg_match("/Total number of bananas: (\d+)/", $ordersContent, $recordedBananaNumber);
+		
+		/* For Debugging */
+		//printf("%d\n",intval($recordedAppleNumber[1]));
+		//printf("%d\n",intval($recordedBananaNumber[1]));
+		//printf("%d\n",intval($recordedOrangeNumber[1]));
+
+		//Get latest number for each fruit
+		$newAppleNumber = $qtyA + (int) $recordedAppleNumber[1]; 
+		$newOrangeNumber = $qtyO+ (int)$recordedOrangeNumber[1];
+		$newBananaNumber = $qtyB + (int)$recordedBananaNumber[1];
+
+		
+		//write back into order.txt with fwrite
+		/*
+		Total number of apples: 0
+		Total number of oranges: 0
+		Total number of bananas: 0
+		*/ 
+		}
+		else{
+			echo "Error locking file1!";
+		}
+
+		//NOT FORGETTING TO CLSE FILE
+		fclose($orderfptr);
+
+		//NOW ON THE WRITING INTO FILE
+		$orderfptr = fopen($filePath,'w')  or exit("Unable to open file!");  // OPEN THE SAME FILE WITH THE INTENTIONG TO WRITE
+		if(flock($orderfptr, LOCK_EX)) //Prevent other from accessing the file	// Check that the lock is exclusive and no other user is writing into it
+		{																			//If it is exclusive then write with fwrite
+			
+			//Write content as per the lab manual
+			fwrite($orderfptr,"Total number of apples: $newAppleNumber\n");			
+			fwrite($orderfptr,"Total number of oranges: $newOrangeNumber\n");
+			fwrite($orderfptr,"Total number of bananas: $newBananaNumber\n");
+
+			flock($fptr, LOCK_UN); //release lock since the writing has been done so others can write into this too
+		}
+		else
+				 echo "Error locking file2!";		// FOR DEBUGGING PURPOSE
+
+		// CLOSE THE FILE AFTER COMPLETING
+		fclose($orderfptr);
+	}
+
+?>
 </body>
 </html>
